023df057f95c67914b6524cc0d8c90802ee3bdee
apane <agustin.pane@gmail.com>
2023-07-26 14:43:43 +0200

    Fix tcrUtils

--- src/utils/tcrUtils.ts
+++ src/utils/tcrUtils.ts
@@ -1,15 +1,26 @@
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { LightGeneralizedTCR } from "../../generated/templates/LightGeneralizedTCR/LightGeneralizedTCR";
 
-export const STATUS_ABSENT = BigInt.fromI32(0);
-export const STATUS_REGISTERED = BigInt.fromI32(1);
-export const STATUS_REGISTRATION_REQUESTED = BigInt.fromI32(2);
-export const STATUS_CLEARING_REQUESTED = BigInt.fromI32(3);
-export const NONE = "None";
-export const ACCEPT = "Accept";
-export const REJECT = "Reject";
-export const REGISTRATION_REQUESTED_CODE = 2;
-export const CLEARING_REQUESTED_CODE = 3;
+// DisputeOutcome "enum" (we cannot use enums in assemblyScript :@!)
+export const DISPUTE_OUTCOME_NONE = "None";
+export const DISPUTE_OUTCOME_ACCEPT = "Accept";
+export const DISPUTE_OUTCOME_REJECT = "Reject";
+//
+
+// DisputeOutcome "enum" (we cannot use enums in assemblyScript x2 :@!!)
+const TheBadgeBadgeStatus_Absent = "Absent";
+const TheBadgeBadgeStatus_Requested = "Requested";
+export const TheBadgeBadgeStatus_Challenged = "Challenged";
+const TheBadgeBadgeStatus_Approved = "Approved";
+const TheBadgeBadgeStatus_RemovalRequested = "RemovalRequested";
+//
+
+// DisputeOutcome "enum" (we cannot use enums in assemblyScript x3 :@!!!)
+const TCRItemStatusCode_ABSENT_CODE = 0;
+const TCRItemStatusCode_REGISTERED_CODE = 1;
+const TCRItemStatusCode_REGISTRATION_REQUESTED_CODE = 2;
+const TCRItemStatusCode_CLEARING_REQUESTED_CODE = 3;
+//
 
 export function getTCRRequestIndex(tcrAddress: Address, itemId: Bytes): BigInt {
   const tcrList = LightGeneralizedTCR.bind(tcrAddress);
@@ -24,8 +35,24 @@ export function getArbitrationParamsIndex(tcrAddress: Address): BigInt {
 }
 
 export function getFinalRuling(outcome: number): string {
-  if (outcome == 0) return NONE;
-  if (outcome == 1) return ACCEPT;
-  if (outcome == 2) return REJECT;
+  if (outcome == 0) return DISPUTE_OUTCOME_NONE;
+  if (outcome == 1) return DISPUTE_OUTCOME_ACCEPT;
+  if (outcome == 2) return DISPUTE_OUTCOME_REJECT;
+  return "Error";
+}
+
+export function getTBStatus(status: number): string {
+  if (status === TCRItemStatusCode_ABSENT_CODE) {
+    return TheBadgeBadgeStatus_Absent;
+  }
+  if (status === TCRItemStatusCode_REGISTERED_CODE) {
+    return TheBadgeBadgeStatus_Approved;
+  }
+  if (status === TCRItemStatusCode_REGISTRATION_REQUESTED_CODE) {
+    return TheBadgeBadgeStatus_Requested;
+  }
+  if (status === TCRItemStatusCode_CLEARING_REQUESTED_CODE) {
+    return TheBadgeBadgeStatus_RemovalRequested;
+  }
   return "Error";
 }
